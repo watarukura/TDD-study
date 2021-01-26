91e5608a2a619259c3dce74c1652180d38aa69e0
wataru-kurashima <wataru-kurashima@hands-lab.com>
2021-01-27 06:35:40 +0900

    feat(franc): francを仮実装

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -3,6 +3,7 @@
 namespace App\Test;
 
 use App\Money\Dollar;
+use App\Money\Franc;
 use PHPUnit\Framework\TestCase;
 
 class MoneyTest extends TestCase
@@ -19,5 +20,12 @@ class MoneyTest extends TestCase
         $five = new Dollar(5);
         self::assertTrue($five->equals(new Dollar(5)));
         self::assertFalse($five->equals(new Dollar(6)));
+    }
+
+    public function testFrancMulitiplication(): void
+    {
+        $five = new Franc(5);
+        self::assertEquals(new Franc(10), $five->times(2));
+        self::assertEquals(new Franc(15), $five->times(3));
     }
 }
