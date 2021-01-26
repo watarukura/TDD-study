732d4484089a075a45cb5c9f188ed88687e7a961
wataru-kurashima <wataru-kurashima@hands-lab.com>
2021-01-27 06:24:34 +0900

    refactor(moneytest): 一次変数$productが不要になったので削除

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -10,10 +10,8 @@ class MoneyTest extends TestCase
     public function testMultiplication(): void
     {
         $five = new Dollar(5);
-        $product = $five->times(2);
-        self::assertEquals(10, $product->amount);
-        $product = $five->times(3);
-        self::assertEquals(15, $product->amount);
+        self::assertEquals(new Dollar(10), $five->times(2));
+        self::assertEquals(new Dollar(15), $five->times(3));
     }
 
     public function testEquality(): void
