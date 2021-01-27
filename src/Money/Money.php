418db05e0c13864bb152461a1dbd855c1c6734a8
wataru-kurashima <wataru-kurashima@hands-lab.com>
2021-01-28 06:22:42 +0900

    feat(money): move equals() to money class

--- src/Money/Money.php
+++ src/Money/Money.php
@@ -6,5 +6,13 @@ namespace App\Money;
 
 class Money
 {
+    /**
+     * @var int $amount
+     */
+    protected $amount;
 
+    public function equals(Money $money): bool
+    {
+        return $this->amount === $money->amount;
+    }
 }
