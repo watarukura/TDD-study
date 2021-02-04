f9099c5b034d339c8047d52e585295c1da647a2d
wataru-kurashima <wataru-kurashima@hands-lab.com>
2021-02-05 06:29:28 +0900

    feat(money): make plus can mixed currencies

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -76,4 +76,14 @@ class MoneyTest extends TestCase
         $bank = new Bank();
         self::assertEquals(1, $bank->rate('USD', 'USD'));
     }
+
+    public function testMixedAddition(): void
+    {
+        $fiveBucks = Money::dollar(5);
+        $tenFrancs = Money::franc(10);
+        $bank = new Bank();
+        $bank->addRate("CHF", "USD", 2);
+        $result = $bank->reduce($fiveBucks->plus($tenFrancs), "USD");
+        self::assertEquals(Money::dollar(10), $result);
+    }
 }
